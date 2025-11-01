7ba76c104fb54a91adf07d558a684b5b56c1593d
MASASHISWHT <sawa11101986@gmail.com>
2025-11-01 16:07:36 +0900

    Add multiply and divide functions

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require('../src/calculator');
+const { add, subtract, multiply, divide } = require('../src/calculator');
 
 test('足し算のテスト', () => {
   expect(add(2, 3)).toBe(5);
@@ -6,4 +6,16 @@ test('足し算のテスト', () => {
 
 test('引き算のテスト', () => {
   expect(subtract(5, 3)).toBe(2);
+});
+
+test('掛け算のテスト', () => {
+  expect(multiply(3, 4)).toBe(12);
+});
+
+test('割り算のテスト', () => {
+  expect(divide(10, 2)).toBe(5);
+});
+
+test('ゼロ除算のテスト', () => {
+  expect(() => divide(10, 0)).toThrow('Cannot divide by zero');
 });
